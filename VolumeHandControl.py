e1a58bd872b3c4578ea36f5c4b2a337b1c6b062e
Varun Achary <84003658+Varun-Ach@users.noreply.github.com>
2021-09-07 23:20:17 +0530

    Add files via upload

---
+++ VolumeHandControl.py
@@ -0,0 +1,80 @@
+import cv2
+import time
+import numpy
+import numpy as np
+
+import HandTrackingModule as htm
+import math
+# install Package pycaw
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+wCam, hCam= 640,480
+cap= cv2.VideoCapture(0)
+cap.set(3,wCam) #propid 3 is width
+cap.set(4,hCam) #propid 4 is height
+pTime=0
+
+detector=htm.handDetector(detconf=0.7)
+
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+#volume.GetMute()
+#volume.GetMasterVolumeLevel()
+volRange=volume.GetVolumeRange()
+volume.SetMasterVolumeLevel(0, None)
+minVol= volRange[0]
+maxVol=volRange[1]
+vol=0
+volBar=400
+volPer=0
+
+while True:
+    success, img= cap.read()
+    img= detector.findHands(img)
+    lmlist= detector.findPosition(img,draw=False)
+    if len(lmlist) != 0:
+        #print(lmlist[4],lmlist[8])
+        # Index 4 is for tip of thumb, Index 8 is for tip of index
+
+        x1,y1= lmlist[4][1],lmlist[4][2] # x & y co-ordinates for tip of thumb
+        x2, y2 = lmlist[8][1], lmlist[8][2] # x & y co-ordinates for tip of index
+        cx,cy = (x1+x2)//2, (y1+y2)//2 # Center of the line
+
+        cv2.circle(img,(x1,y1),10,(255,0,255),cv2.FILLED)
+        cv2.circle(img, (x2, y2), 10, (255, 0, 255), cv2.FILLED)
+        cv2.line(img,(x1,y1),(x2,y2),(255,0,255),3)
+        cv2.circle(img, (cx, cy), 9, (255, 0, 255), cv2.FILLED)
+
+        length= math.hypot(x2-x1,y2-y1)
+        #print(length)
+
+        # Hand range= 25 - 175
+        # Volume range = -65 - 0
+        vol= np.interp(length,[25,175],[minVol,maxVol])
+        volBar = np.interp(length, [25, 175], [400, 150])
+        volPer = np.interp(length, [25, 175], [0, 100])
+        print(vol)
+        volume.SetMasterVolumeLevel(vol, None)
+
+
+        if length<30:
+            cv2.circle(img, (cx, cy), 9, (0, 255, 0), cv2.FILLED)
+
+    cv2.rectangle(img,(50,150),(85,400),(0,255,0),3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (0, 255, 0), cv2.FILLED)
+    cv2.putText(img, f'{int(volPer)} % ', (40, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+
+
+
+    cTime=time.time()
+    fps=1/(cTime-pTime)
+    pTime=cTime
+    cv2.putText(img,f'FPS: {int(fps)}',(40,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),3)
+
+    cv2.imshow("img",img)
+    cv2.waitKey(1)
